65e5cbec4132e5418fea114fd680201b1b65498f
Ian Ansell <ian.ansell.uk@gmail.com>
2023-04-17 14:39:04 +0100

    set up general project structure

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.10-slim-buster
+
+LABEL version="1.0"
+LABEL maintainer="Data Services"
+
+COPY . /circuits
+WORKDIR /circuits
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+RUN chmod 444 *.py
+RUN chmod 444 requirements.txt
+
+ENV PORT 8080
+
+EXPOSE 8080
+
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 wsgi:app
---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+port = os.environ.get('PORT')
+
+@app.route('/', methods=['GET'])
+def home():
+    return "home"
+
+@app.route('/placeholder', methods=['GET'])
+def placeholder():
+    return render_template(
+        'index.html', title='Home')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
